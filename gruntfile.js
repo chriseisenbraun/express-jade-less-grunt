5aeb6870641fa32ae9fd61287be5a86ffa8a1b8d
Chris Eisenbraun <hello@chriseisenbraun.com>
2014-07-02 21:41:54 -0700

    Add DSS to gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -21,4 +21,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.registerTask('default', 'watch');
+  grunt.loadNpmTasks('grunt-dss');
 };
